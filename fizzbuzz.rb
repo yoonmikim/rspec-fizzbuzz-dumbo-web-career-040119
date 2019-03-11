25a667304b733e1139cecea56278626757d1ef1a
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-11 03:16:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ require 'pry'
 def fizzbuzz(n)
   if n % 3 == 0 && n % 5 == 0
     'FizzBuzz'
-  elsif n % 3 == 0 
+  elsif n % 3 == 0 && n % 5 != 0
     'Fizz'
   elsif n % 5 == 0 && n % 3 != 0
     'Buzz'
