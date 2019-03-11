5a654da8433a4be53505264e531d95e9f92a95f9
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-11 03:13:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0
+    'fizzbuzz'
+  elsif n % 3 == 0
+    'fizz'
+  elsif n % 5 == 0
+    'buzz'
+  else
+    'nil'
 end
